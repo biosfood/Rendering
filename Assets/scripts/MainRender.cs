0f8eacdc957d1edff9717a982c44047aeebf1221
Lukas <leisenhau@gmail.com>
2023-04-12 08:48:43 +0200

    initial commit

---
+++ Assets/scripts/MainRender.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainRender : MonoBehaviour {
+    private RenderTexture renderTexture;
+    public ComputeShader display, raytrace;
+    private int doDisplay, doReset, doRender;
+    public int samplesPerUpdate = 1;
+    public int samples = 0;
+    public int width = 400, height = 400;
+    private ComputeBuffer light;
+    private bool invalidate = true;
+    private bool ready = false;
+
+    void Start() {
+        doDisplay = display.FindKernel("display");
+        doRender = raytrace.FindKernel("trace");
+        doReset = raytrace.FindKernel("reset");
+        transform.hasChanged = true;
+    }
+
+    void Update() {
+        if (transform.hasChanged) {
+            transform.hasChanged = false;
+            raytrace.SetVector("position", transform.position);
+            raytrace.SetVector("viewDirection", transform.forward);
+            invalidate = true;
+        }
+        if (invalidate) {
+            raytrace.SetInt("width", width);
+            raytrace.SetInt("height", height);
+            display.SetInt("width", width);
+
+            if (light != null) {
+                light.Release();
+            }
+            light = new ComputeBuffer(width * height, 12);
+            display.SetBuffer(doDisplay, "light", light);
+            raytrace.SetBuffer(doRender, "light", light);
+            raytrace.SetBuffer(doReset, "light", light);
+            raytrace.Dispatch(doReset, width/8, height/8, 1);
+
+            if (renderTexture != null) {
+                renderTexture.Release();
+            }
+            renderTexture = new RenderTexture(width, height, 0);
+            renderTexture.enableRandomWrite = true;
+            renderTexture.Create();
+            display.SetTexture(doDisplay, "destination", renderTexture);
+
+            samples = 0;
+            invalidate = false;
+            ready = true;
+        }
+        if (!ready) return;
+        samples += samplesPerUpdate;
+        for (int i = 0; i < samplesPerUpdate; i++) {
+            raytrace.Dispatch(doRender, width/8, height/8, 1);
+        }
+    }
+
+    private void OnRenderImage(RenderTexture before, RenderTexture destination) {
+        if (width != before.width) {
+            width = before.width;
+            invalidate = true;
+        }
+        if (height != before.height) {
+            height = before.height;
+            invalidate = true;
+        }
+        if (invalidate) {
+            return;
+        }
+        display.SetFloat("scale", 1f / samples);
+        display.Dispatch(doDisplay, width / 8, height / 8, 1);
+        Graphics.Blit(renderTexture, destination); 
+    }
+}
